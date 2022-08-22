feb5b7de6a14c65a95948000712714253974f9b5
ArmanKhoyetsyan <arman.khoyetsyan.dwin@gmail.com>
2022-08-22 14:05:03 +0400

    s

--- backend/index.js
+++ backend/index.js
@@ -6,8 +6,6 @@ const app = express();
 const port = process.env.PORT || 3030;
 app.use(cors())
 
-app.get('/user', getUsers)
+//app.get('/user', getUsers)
 
 app.listen(port, () => console.log(`Port${port}`))
-
-
