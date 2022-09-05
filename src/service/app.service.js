38aceca2917988ce044f40c29e5e36ce2ed44bf5
ArmanKhoyetsyan <arman.khoyetsyan.dwin@gmail.com>
2022-09-05 14:56:32 +0400

    code refactoring

--- src/service/app.service.js
+++ src/service/app.service.js
@@ -17,33 +17,4 @@ async function loginBtClick({ userName, password }) {
     return response;
 }
 
-// async function getMessages({ firstUserName, secondUserName }) {
-//     const data = JSON.stringify({
-//         'firstUserName': firstUserName,
-//         'secondUserName': secondUserName
-//     });
-//     const myHeaders = new Headers({
-//         'Content-Type': 'application/json',
-//         'api_key': API_KEY
-//     });
-//     const response = await fetch('http://192.168.31.183:3030/getMessages', {
-//         body: data,
-//         method: 'GET',
-//         headers: myHeaders,
-//     });
-//     return response;
-// }
-
-// async function getGroups() {
-//     const myHeaders = new Headers({
-//         'Content-Type': 'application/json',
-//         'api_key': API_KEY
-//     });
-//     const response = await fetch('http://localhost:3030/groups', {
-//         method: 'GET',
-//         headers: myHeaders,
-//     });
-//     return response;
-// }
-
 export { loginBtClick }
